1a0c01d475d7f233aeece879c469146bdc56d62e
fgusztav <ferenczi.gusztav@gmail.com>
2022-03-23 20:33:11 +0100

    Virológus osztály metódusai üresen megírva, JavaDoc kommentekkel.

--- Szkeleton/src/Virologus.java
+++ Szkeleton/src/Virologus.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A virológus a karakter amit a játékos mozgat. Tárolja, hogy melyik mezőn
+ * van éppen. Fel tud venni és le tud adni valamint lophat is védőfelszerelést és
+ * anyagokat. Meg tud tanulni Hatásokat azaz kódokat. Minden körben lép. És le tud
+ * bénulni ilyenkor nem mozog és a nála lévő anyagok elvehetőek.
+ */
 public class Virologus {
     private Mezo mezo;
     private Anyag tarolo;
@@ -21,9 +27,99 @@ public class Virologus {
         this.maxAnyag = maxAnyag;
     }
 
+    /**
+     * A cél virológusra felkeni a kiválasztott ágenst.
+     * @param cel Virologus akit kennek.
+     * @param agens Ágens, amivel kennek.
+     */
+    public void kenes(Virologus cel, Agens agens) {
+
+    }
+
+    /**
+     * Ha az adott virológust megkenik, akkor ez a függvény hívodik meg, és az adott ágens bekerül a "rakenve" listájába.
+     * @param agens Az ágens, amivel kenték a virológust.
+     */
+    public void megkenve(Agens agens) {
+
+    }
+
+    /**
+     * új kódot ismer meg a virológus
+     * @param k Letapogatott kód.
+     */
+    public void kod_hozzaad(Kod k) {
+
+    }
+
+    /**
+     * Új felszerelést vesz fel.
+     * @param f Felvett felszerelés.
+     */
+    public void felszereles_hozzaad(Felszereles f) {
+
+    }
+
+    /**
+     * A virológus leadja a felszerelést.
+     * @param f Leadott felszerelés.
+     */
+    public void felszereles_leadas(Felszereles f) {
+
+    }
+
+    /**
+     * A virológus új mezőre ugrik.
+     * @param m Az a mező, amelyre a virológust áthelyezzük.
+     */
+    public void mozgas(Mezo m) {
+
+    }
+
+    /**
+     *  A léptethető függvénye, minden körben lép a virológus/körönként meghívjuk.
+     */
+    public void lep() {
+
+    }
+
+    /**
+     * Felvesz anyagot a raktárból (amennyit tud).
+     */
+    public void anyag_felvesz() {
+
+    }
+
+    /**
+     * Visszaadja, hogy megvehető-e az ágens.
+     * @param k Kód, amelynek az árát ellenőrizzük.
+     * @return Megvehető-e az ágens (true/false).
+     */
+    public boolean check_ar(Kod k) {
+
+        return false;
+    }
+
+    /**
+     * Ismert kódok (ismert_hatasok lista) alapján lérehozunk egy ágenst, a megfelelő anyagmennyiséget felhasználva.
+     * @param k Kód, amelyből ágenst szeretnénk lélrehozni.
+     */
+    public void agens_letrehoz(Kod k) {
+
+    }
+
+    /**
+     * Egy adott virológus és zsákja anyagtároló kapacitása szerint a lehető maximális anyagmennyiséggel megtölti annak tárolóit,
+     * a saját anyagmennyiséget annyival csökkentve (bénultság esetén lopásnál).
+     * @param v Virológus, aki megkapja az anyagot.
+     */
+    public void megtolt_tarolo(Virologus v) {
+
+    }
 
 
 
+    //Getterek-setterek:
     public Mezo getMezo() {
         return mezo;
     }
