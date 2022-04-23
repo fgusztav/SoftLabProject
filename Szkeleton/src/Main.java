99cdd2630ab86cb33b24dfa18c794e2a382f4947
fgusztav <ferenczi.gusztav@gmail.com>
2022-04-23 20:42:11 +0200

    Main atirva, minimain, teszt mod elkezdve

--- Szkeleton/src/Main.java
+++ Szkeleton/src/Main.java
@@ -4,51 +4,57 @@ public class Main {
     public static GameManager gm = new GameManager(4);
 
     public static void main(String[] args) {
-        String parancs = new String(); //csak példaként az első parancs
-        String parameter = new String(); // masodik parancs
-        Leironyelv interfesz = new Leironyelv();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String bemenet = sc.nextLine();
+            String parancs = bemenet.split(" ")[0];
+            //String parancs = new String(); //csak példaként az első parancs
+            String parameter = bemenet.split(" ", 1)[1];
+            //String parameter = new String(); // masodik parancs
+            Leironyelv interfesz = new Leironyelv();
 
-        interfesz.list("virologus");
+            interfesz.list("virologus");
 
-        switch (parancs) {
-            case "create":
-                interfesz.create(parameter);
-            case "help":
-                interfesz.create(parameter);
-            case "info":
-                interfesz.create(parameter);
-            case "put":
-                interfesz.create(parameter);
-            case "list":
-                interfesz.create(parameter);
-            case "load":
-                interfesz.create(parameter);
-            case "save":
-                interfesz.create(parameter);
-            case "move":
-                interfesz.create(parameter);
-            case "print_to_file":
-                interfesz.create(parameter);
-            case "give":
-                interfesz.create(parameter);
-            case "step":
-                interfesz.create(parameter);
-            case "generate_map":
-                interfesz.create(parameter);
-            case "smear_virus":
-                interfesz.create(parameter);
-            case "kill":
-                interfesz.create(parameter);
-            case "make_agent":
-                interfesz.create(parameter);
-            case "random":
-                interfesz.create(parameter);
-            case "clear":
-                interfesz.create(parameter);
-            case "defensivecoat":
-                interfesz.create(parameter);
-            case "test_mode":
-                interfesz.create(parameter);
+            switch (parancs) {
+                case "create":
+                    interfesz.create(parameter);
+                case "help":
+                    interfesz.help(parameter);
+                case "info":
+                    interfesz.info(parameter);
+                case "put":
+                    interfesz.put(parameter);
+                case "list":
+                    interfesz.list(parameter);
+                case "load":
+                    interfesz.load(parameter);
+                case "save":
+                    interfesz.save(parameter);
+                case "move":
+                    interfesz.move(parameter);
+                case "print_to_file":
+                    interfesz.print_to_file(parameter);
+                case "give":
+                    interfesz.give(parameter);
+                case "step":
+                    interfesz.step(parameter);
+                case "generate_map":
+                    interfesz.generate_map(parameter);
+                case "smear_virus":
+                    interfesz.smear_virus(parameter);
+                case "kill":
+                    interfesz.kill(parameter);
+                case "make_agent":
+                    interfesz.make_agent(parameter);
+                case "random":
+                    interfesz.random(parameter);
+                case "clear":
+                    interfesz.clear(parameter);
+                case "defensivecoat":
+                    interfesz.defensivecoat(parameter);
+                case "test_mode":
+                    interfesz.test_mode(parameter);
+            }
         }
     }
 }
